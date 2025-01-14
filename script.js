366419c57093d856225dacc49068d765f462fd1d
Ogotlhe Tsheko <tshekoogotlhe101@gmail.com>
2025-01-14 10:28:36 +0200

    Added icons

--- script.js
+++ script.js
@@ -12,8 +12,27 @@ const addTask = () => {
 
 const updateTasksList = () => {
     const tasksList = document.getElementById('task-list');
-    tasksList.innerHTML = '';
-}
+    tasksList.innerHTML = "";
+
+    tasks.forEach((task,index) => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `
+        <div class="taskItem">
+             <div class="task">
+              <input type="checkbox" class="checkbox"/>
+              <p>Finish this project</p>
+              </div>
+              <div class="icons">
+            <i class="fa-light fa-pen-to-square"></i>
+             <i class="fa-solid fa-trash" style="color: #a13030;"></i> 
+              </div>
+            </div>
+            `;
+            listItem.addEventListener('change',()=> toggleTaskComplete(index));
+          tasksList.append(listItem);
+        
+    });
+};
 
 document.getElementById('newTask').addEventListener('click', function(e) {
     e.preventDefault();
